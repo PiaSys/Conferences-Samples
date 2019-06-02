615a4ae5d29fa48d3b91b3223d9e19624adf69c3
paolo@pialorsi.com <paolo@pialorsi.com>
2019-06-02 08:22:26 +0100

    Added PowerShell script to extract modern pages

---
+++ pnp-powershell-export-modern-pages/Play-With-Modern-Sites-Pages-Content-Free.ps1
@@ -0,0 +1,33 @@
+﻿# Connect to the target site
+Connect-PnPOnline "https://<tenant>.sharepoint.com/sites/<source_site>/"
+
+# Define global constants
+$pnp = [System.Xml.Linq.XNamespace]"http://schemas.dev.office.com/PnP/2019/03/ProvisioningSchema"
+
+# Get the site template as an XML DOM document
+$templateXml = Get-PnPProvisioningTemplate -ExcludeHandlers Navigation,WebSettings,RegionalSettings,PropertyBagEntries,SiteSecurity
+$template = [System.Xml.Linq.XElement]::Parse($templateXml)
+$clientSidePages = $template.Element($pnp + [System.Xml.Linq.XName]"Templates").Element($pnp + [System.Xml.Linq.XName]"ProvisioningTemplate").Element($pnp + [System.Xml.Linq.XName]"ClientSidePages")
+
+# Defines the list of pages to export
+$pages = @(
+    "SampleNews01.aspx", 
+    "SampleNews02.aspx"
+    )
+
+# Add every single page to the target template
+foreach ($page in $pages) {
+
+    # Export the page as an XML DOM document
+    $pageXml = [xml](Export-PnPClientSidePage -Identity $page)
+    $pageXElement = [System.Xml.Linq.XElement]::Parse($pageXml.Provisioning.Templates.ProvisioningTemplate.ClientSidePages.ClientSidePage.OuterXml)
+
+    # Add the page to the template
+    $clientSidePages.Add($pageXElement)
+}
+
+$template.Save("c:\temp\DemoComSite.xml")
+notepad c:\temp\DemoComSite.xml
+
+Connect-PnPOnline "https://<tenant>.sharepoint.com/sites/<target_site>/"
+Apply-PnPProvisioningTemplate -Path c:\temp\DemoComSite.xml
