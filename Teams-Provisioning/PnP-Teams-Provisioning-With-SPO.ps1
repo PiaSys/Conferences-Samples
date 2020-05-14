f5a1115098d8c7e7fafdd2a957e4c19bcfb55152
paolo@pialorsi.com <paolo@pialorsi.com>
2020-05-14 16:56:54 +0200

    Added Teams provisioning samples

---
+++ Teams-Provisioning/PnP-Teams-Provisioning-With-SPO.ps1
@@ -0,0 +1,6 @@
+﻿# Connect to the target tenant with specific user's identity via Credential Manager
+Connect-PnPOnline "https://<tenant>.sharepoint.com/" -Credentials <CredentialsName>
+
+# Apply the tenant template
+Apply-PnPTenantTemplate -Path .\demo-team-template-with-spo.xml `
+    -Parameters @{"TeamTitle"="PnPTeamDemoSIG01C";"TeamAlias"="pnpteamdemosig01c";"CompanyName"="PnP"}
