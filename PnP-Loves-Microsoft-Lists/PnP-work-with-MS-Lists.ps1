e33aa3d93c7bf85ebc4173202aa8b27339cd0578
Paolo Pialorsi <paolo@pialorsi.com>
2020-09-16 15:33:18 +0200

    Added samples for video PnP loves Microsoft Lists

---
+++ PnP-Loves-Microsoft-Lists/PnP-work-with-MS-Lists.ps1
@@ -0,0 +1,28 @@
+﻿#************************************************************************************************
+# Play with "personal lists" in the target user's My Site (OneDrive for Business)
+#************************************************************************************************
+
+# Connect to the OneDrive for Business site of the user
+Connect-PnPOnline "https://<tenant>-my.sharepoint.com/personal/<user-personal-site>/" 
+
+# Get the "Issue Tracker" list
+$list = Get-PnPList "Issue tracker"
+
+# Get the content types of the list
+Get-PnPProperty -ClientObject $list -Property ContentTypes
+
+# Add a new item into the list
+Add-PnPListItem -List $list -ContentType $list.ContentTypes[0].Id.StringValue -Values @{"Title"="Inserted via PnP";"Status"="Completed"}
+
+#************************************************************************************************
+# Play with "site lists"
+#************************************************************************************************
+Connect-PnPOnline "https://<tenant>.sharepoint.com/sites/<source-site-url>/"
+
+# Extract the template of a list of Microsoft Lists that you previously created
+Get-PnPProvisioningTemplate -Out .\employee.xml -Handlers Lists -ListsToExtract "Employee onboarding"
+
+# Connect to another site, which will be the target of the provisioning
+Connect-PnPOnline "https://<tenant>.sharepoint.com/sites/<target-site-url>/"
+
+Apply-PnPProvisioningTemplate -Path .\employee.xml
