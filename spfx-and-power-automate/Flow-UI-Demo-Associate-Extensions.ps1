f597a8b940fca1481bef2fc12ad994d8debf796f
Paolo Pialorsi <paolo@pialorsi.com>
2020-11-19 16:47:31 +0100

    SPFx with Power Automate demo

--- spfx-and-power-automate/Flow-UI-Demo-Associate-Extensions.ps1
+++ spfx-and-power-automate/Flow-UI-Demo-Associate-Extensions.ps1
@@ -1,12 +1,3 @@
-﻿Connect-PnPOnline "https://piasysdev.sharepoint.com/sites/Demo-SPFx-Flow-Panel/" -Credentials PiaSysDev-Paolo
-Get-PnPCustomAction
+﻿Connect-PnPOnline "https://<tenant>.sharepoint.com/sites/<site>/" 
+
 Add-PnPCustomAction -Name "StartFlowWithArguments" -Title "StartFlowWithArguments" -RegistrationId 101 -RegistrationType List -Location "ClientSideExtension.ListViewCommandSet.CommandBar" -ClientSideComponentId e37896bc-97ad-4676-b65a-23e334da1420 -ClientSideComponentProperties "{'targetContentTypeId':'0x0101001EA52A6C6B4DB548825829F35F9B87BB'}"
-
-$action = Get-PnPCustomAction -Identity 66253949-5f1f-4f0c-bf4f-2db8754dc9ff
-$action.ClientSideComponentId
-$action.ClientSideComponentProperties
-$action.Location
-$action.RegistrationId
-$action.RegistrationType
-
-Remove-PnPCustomAction -Identity 66253949-5f1f-4f0c-bf4f-2db8754dc9ff
