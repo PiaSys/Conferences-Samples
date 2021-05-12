e5ebf348d2a817c2e56e3d0732aac5c539a217fe
Paolo Pialorsi <paolo@pialorsi.com>
2021-05-12 18:55:33 +0200

    Improved query and paging samples for PnP Core SDK

--- PnP-Core-SDK/PnPCoreSDKQuerModel01/Program.cs
+++ PnP-Core-SDK/PnPCoreSDKQuerModel01/Program.cs
@@ -46,6 +46,12 @@ namespace PnPCoreSDKAuthDemo
 
                 using (var context = await pnpContextFactory.CreateAsync("TestSite"))
                 {
+                    #region Query Model Demo
+
+                    /******************************************
+                    * QUERY MODEL DEMO
+                    *******************************************/
+
                     // Here we load the selected properties into the current web object
                     await context.Web.LoadAsync(p => p.Title, p => p.Lists);
 
@@ -57,47 +63,47 @@ namespace PnPCoreSDKAuthDemo
                     {
                         Console.WriteLine($"{l.Id} - {l.Title}");
                     }
-                    Console.ReadLine();
+                    Wait();
 
                     // Otherwise, whenever you query or browse (foreach) a collection, it will trigger a LINQ query
                     foreach (var l in context.Web.Lists)
                     {
                         Console.WriteLine($"{l.Id} - {l.Title}");
                     }
-                    Console.ReadLine();
+                    Wait();
 
                     // This is a LINQ query
                     var queryLists = context.Web.Lists
                         .OrderBy(l => l.Title)
-                        .QueryProperties(l => l.Id, l => l.Title);
+                        .QueryProperties(l => l.Id, l => l.Title); // This is a custom extension method that we introduced
 
-                    // We actually execute the query when we go through all of its items (foreach)
+                    // We actually execute the query when we go through all of its items (foreach = GetEnumerator)
                     foreach (var l in queryLists)
                     {
                         Console.WriteLine($"{l.Id} - {l.Title}");
                     }
-                    Console.ReadLine();
+                    Wait();
 
                     // Or you can use the LINQ query syntax
-                    // This is a LINQ query
+                    // This is another LINQ query
                     var queryListsLinq = 
                         (from l in context.Web.Lists
                             where l.TemplateType == PnP.Core.Model.SharePoint.ListTemplateType.DocumentLibrary
                             orderby l.Title
                             select l)
-                        .QueryProperties(l => l.Id, l => l.Title);
+                        .QueryProperties(l => l.Id, l => l.Title); // The extension method is not available in LINQ syntax
 
-                    // We actually execute the query when we go through all of its items (foreach)
+                    // We actually execute the query when we go through all of its items (foreach = GetEnumerator)
                     foreach (var l in queryListsLinq)
                     {
                         Console.WriteLine($"{l.Id} - {l.Title}");
                     }
-                    Console.ReadLine();
+                    Wait();
 
-                    // Here load the selected  properties into a new web object
+                    // Here we get a new web object and we load the selected properties into it
                     var web = await context.Web.GetAsync(
                         p => p.Title,
-                        p => p.Lists.QueryProperties(l => l.Id, l => l.Title));
+                        p => p.Lists.QueryProperties(l => l.Id, l => l.Title)); // Notice the nested QueryProperties
 
                     Console.WriteLine(web.Title);
 
@@ -106,8 +112,96 @@ namespace PnPCoreSDKAuthDemo
                     {
                         Console.WriteLine($"{l.Id} - {l.Title}");
                     }
+                    Wait();
+
+                    // Add a request to the current batch
+                    var webBatched = await context.Web.GetBatchAsync(context.CurrentBatch, w => w.Id, w => w.Title);
+
+                    // You can query data using explicit batching, too
+                    var queryListsLinqBatch =
+                        await context.Web.Lists
+                        .OrderBy(l => l.Title)
+                        .QueryProperties(l => l.Id, l => l.Title)
+                        .AsBatchAsync(); // Adds the request to the current batch
+
+                    Console.WriteLine($"Is the batched query available? {queryListsLinqBatch.IsAvailable}");
+                    Wait();
+
+                    // Execute the batch
+                    await context.ExecuteAsync();
+
+                    Console.WriteLine($"Is the batched query available? {queryListsLinqBatch.IsAvailable}");
+
+                    foreach (var l in queryListsLinqBatch)
+                    {
+                        Console.WriteLine($"{l.Id} - {l.Title}");
+                    }
+                    Wait();
+
+                    #endregion
+
+                    #region Paging Demo
+
+                    /******************************************
+                    * PAGING DEMO                     
+                    *******************************************/
+
+                    // Let's create a new channel for demo purposes
+
+                    // Get a channel with some messages
+                    var channelForPaging = await context.Team.Channels.FirstOrDefaultAsync(c => c.DisplayName == "Paging test 877851905");
+
+                    // Full load of messages
+                    await channelForPaging.LoadAsync(p => p.Messages);
+                    Console.WriteLine($"We've got {channelForPaging.Messages.Length} messages");
+                    Wait();
+
+                    // Implicit async paging (this is the best option!)
+                    await foreach (var m in channelForPaging.Messages)
+                    {
+                        Console.WriteLine($"{m.Id}: {m.Body.Content}");
+                    }
+                    Wait();
+
+                    // Implicit sync paging (not optimal, but still really simple)
+                    foreach (var m in channelForPaging.Messages)
+                    {
+                        Console.WriteLine($"{m.Id}: {m.Body.Content}");
+                    }
+                    Wait();
+
+                    // Manual paging
+
+                    int pageCount = 0;
+                    int pageSize = 10;
+
+                    while (true)
+                    {
+                        var page = channelForPaging.Messages.Skip(pageSize * pageCount).Take(pageSize).ToArray();
+
+                        // Use the current page ...
+                        foreach (var m in page)
+                        {
+                            Console.WriteLine($"{m.Id}: {m.Body.Content}");
+                        }
+
+                        pageCount++;
+                        if (page.Length < pageSize)
+                        {
+                            break;
+                        }
+                    }
+                    Wait();
+
+                    #endregion
                 }
             }
+        }
+
+        static void Wait()
+        {
+            Console.WriteLine("Press ENTER to continue ...");
+            Console.ReadLine();
         }
     }
 }
