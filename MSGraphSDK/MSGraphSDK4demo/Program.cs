31482ff2ee942a177e92e63bddbb0be67eb03e87
Paolo Pialorsi <paolo@pialorsi.com>
2021-08-03 19:00:21 +0200

    Graph + Azure.Identity sample

--- MSGraphSDK/MSGraphSDK4demo/Program.cs
+++ MSGraphSDK/MSGraphSDK4demo/Program.cs
@@ -10,14 +10,88 @@ namespace MSGraphSDK4demo
     {
         static async Task Main(string[] args)
         {
+            await UseClientSecretCredentialAsync();
+            await UseInteractiveBrowserCredentialAsync();
+            await UseDefaultAzureCredentialAsync();
+        }
+
+        static async Task UseClientSecretCredentialAsync()
+        {
+            // Define the permission scopes that you need
+            string[] scopes = { ".default" };
+
+            #region Client Credentials
+
+            var tenantId = "<client-id>";
+            var clientId = "<tenant-id>";
+            var clientSecret = "<client-secret>";
+
+            #endregion
+
+            // And a TokenCredential implementation
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId,
+                clientId,
+                clientSecret);
+
+            // GraphServiceClient now supports as TokenCredential input
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+            // Use regular Graph SDK fluent syntax
+            var me = await graphClient.Users["paolo@piasysdev.onmicrosoft.com"].Request().GetAsync();
+            Console.WriteLine(me.DisplayName);
+        }
+
+        static async Task UseInteractiveBrowserCredentialAsync()
+        {
+            // Define the permission scopes that you need
+            string[] scopes = { "User.Read" };
+
+            var options = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = "<client-id>",
+                TenantId = "<tenant-id>",
+                RedirectUri = new Uri("http://localhost")
+            };
+
+            // And a TokenCredential implementation
+            var interactiveCredential = new InteractiveBrowserCredential(options);
+
+            // GraphServiceClient now supports as TokenCredential input
+            var graphClient = new GraphServiceClient(interactiveCredential, scopes);
+
+            // Use regular Graph SDK fluent syntax
+            var me = await graphClient.Me.Request().GetAsync();
+            Console.WriteLine(me.DisplayName);
+
+        }
+
+        static async Task UseDefaultAzureCredentialAsync()
+        {
+            // Define the permission scopes that you need
+            string[] scopes = { ".default" };
+
+            // And a TokenCredential implementation
+            var azureCredential = new DefaultAzureCredential(true);
+
+            // GraphServiceClient now supports as TokenCredential input
+            var graphClient = new GraphServiceClient(azureCredential, scopes);
+
+            // Use regular Graph SDK fluent syntax
+            var me = await graphClient.Me.Request().GetAsync();
+            Console.WriteLine(me.DisplayName);
+        }
+
+        static async Task ConsumeGraphAsync()
+        {
             // Define the permission scopes that you need
             string[] scopes = { "User.Read", "Files.Read.All", "Group.Read.All" };
 
             // Using Azure.Identity library, configure a set of credential options
             DeviceCodeCredentialOptions deviceCodeCredentialOptions = new DeviceCodeCredentialOptions()
             {
-                ClientId = "<clientId>",
-                TenantId = "<tenantId>",
+                ClientId = "<client-id>",
+                TenantId = "<tenant-id>",
                 DeviceCodeCallback = async (deviceCodeInfo, cancellationToken) =>
                 {
                     // Write the Device Code in the Output window
