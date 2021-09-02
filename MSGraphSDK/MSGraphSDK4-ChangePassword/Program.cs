420c6e2ec6b8056c47334562d1b57a069a05ead9
Paolo Pialorsi <paolo@pialorsi.com>
2021-09-02 10:18:59 +0200

    Added Graph ChangePassword sample

---
+++ MSGraphSDK/MSGraphSDK4-ChangePassword/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MSGraphSDK4ChangePassword
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Define the permission scopes that you need
+            string[] scopes = { "Directory.AccessAsUser.All" };
+
+            // Start interactive login session
+            var options = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = "<client-id>",
+                TenantId = "<tenant-id>",
+                RedirectUri = new Uri("http://localhost")
+            };
+
+            // And a TokenCredential implementation
+            var interactiveCredential = new InteractiveBrowserCredential(options);
+
+            // Create GraphServiceClient instance
+            var graphClient = new GraphServiceClient(interactiveCredential, scopes);
+
+            // Set variable (in real solution you should rely on UI to collect passwords in a secure way)
+            var currentPassword = "currentPassword";
+            var newPassword = "newPassword";
+
+            // Change current user's password
+            await graphClient.Me.ChangePassword(currentPassword, newPassword).Request().PostAsync();
+            Console.WriteLine("Password changed!");
+        }
+    }
+}
--- TeamsFeedNotification/Program.cs
+++ TeamsFeedNotification/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Identity;
 using Microsoft.Graph;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace TeamsFeedNotification
@@ -18,9 +19,19 @@ namespace TeamsFeedNotification
             // Create a GraphServiceClient instance with the defined TokenCredential
             var graphClient = new GraphServiceClient(azureCredential, scopes);
 
+            var templateParameters = new List<Microsoft.Graph.KeyValuePair>()
+            {
+                new Microsoft.Graph.KeyValuePair
+                {
+                    Name = "sender",
+                    Value = "DemoApp"
+                }
+            };
+
             try
             {
-                await graphClient.Users["paolo@piasysdev.onmicrosoft.com"]
+                // Notification sent to a specific user
+                await graphClient.Users["paolo@piasysdev.onmicrosoft.com"] // Myself
                     .Teamwork
                     .SendActivityNotification(
                         topic: new TeamworkActivityTopic {
@@ -28,14 +39,37 @@ namespace TeamsFeedNotification
                             Value = "Here is a notification for you!",
                             WebUrl = "https://teams.microsoft.com/l/message/19%3Ae2cd01a2b97e4f9eae1a8d76e65780f0%40thread.tacv2/1628084133575?groupId=face8309-4630-4213-a2da-513c1aa39046&tenantId=6c94075a-da0a-4c6a-8411-badf652e8b53&createdTime=1628084133575&parentMessageId=1628084133575"
                         },
-                        activityType: "CustomNotification",
+                        activityType: "customNotification",
                         previewText: new ItemBody
                         {
                             Content = "Preview of the notification!"
-                        }                        
+                        },
+                        templateParameters: templateParameters
                     ).Request().PostAsync();
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Notification sent!");
+                Console.WriteLine("Notification sent to User!");
+
+                await graphClient.Teams["face8309-4630-4213-a2da-513c1aa39046"]
+                    .SendActivityNotification(
+                        topic: new TeamworkActivityTopic
+                        {
+                            Source = TeamworkActivityTopicSource.Text,
+                            Value = "Here is a notification for you!",
+                            WebUrl = "https://teams.microsoft.com/l/message/19%3Ae2cd01a2b97e4f9eae1a8d76e65780f0%40thread.tacv2/1628084133575?groupId=face8309-4630-4213-a2da-513c1aa39046&tenantId=6c94075a-da0a-4c6a-8411-badf652e8b53&createdTime=1628084133575&parentMessageId=1628084133575"
+                        },
+                        activityType: "customNotification",
+                        previewText: new ItemBody
+                        {
+                            Content = "Preview of the notification!"
+                        },
+                        templateParameters: templateParameters,
+                        recipient: new AadUserNotificationRecipient
+                        {
+                            UserId = "bea7a848-0459-4bee-9034-5513ee7f66e0" // Myself
+                        }
+                    ).Request().PostAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Notification sent to user in a Team!");
             }
             catch (Exception ex)
             {
