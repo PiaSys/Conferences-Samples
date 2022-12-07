07ec07e82dfb7645642727160b5c0a063e57a6e8
Paolo Pialorsi <paolo@pialorsi.com>
2022-12-06 16:32:38 -0800

    Updated m365conf samples

--- m365conf/provisioning/extract-template.ps1
+++ m365conf/provisioning/extract-template.ps1
@@ -1,3 +1,3 @@
 # For reference only: https://learn.microsoft.com/en-us/sharepoint/dev/solution-guidance/configuring-the-pnp-provisioning-engine
-Connect-PnPOnline https://piasysdev.sharepoint.com/sites/M365Conf2022/ -Credentials PiaSysDev-Paolo
+Connect-PnPOnline https://source-tenant.sharepoint.com/sites/M365Conf2022/
 Get-PnPSiteTemplate -Configuration ./extract-config.json -Out events-template.xml
