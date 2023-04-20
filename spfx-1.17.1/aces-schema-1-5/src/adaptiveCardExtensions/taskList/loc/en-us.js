3e9dccbe4ce531b56ac29f8b8f2eef91db2ce56c
Paolo Pialorsi <paolo@pialorsi.com>
2023-04-20 13:13:38 +0200

    Added ACE with new Table element

---
+++ spfx-1.17.1/aces-schema-1-5/src/adaptiveCardExtensions/taskList/loc/en-us.js
@@ -0,0 +1,11 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Write 1-3 sentences describing the functionality of this component.",
+    "TitleFieldLabel": "Card title",
+    "Title": "Demo list of ToDo Tasks",
+    "SubTitle": "Here is the list of your tasks",
+    "PrimaryText": "SPFx Adaptive Card Extension",
+    "Description": "Create your SPFx Adaptive Card Extension solution!",
+    "QuickViewButton": "Quick view"
+  }
+});
