9243d84bb70712afbbc8958dbc3da185af8ed78a
Paolo Pialorsi <paolo@pialorsi.com>
2023-04-27 12:41:23 +0200

    Added Web Part Top Actions demo

---
+++ spfx-top-actions-1.17.1/src/webparts/topActionsDemo/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ITopActionsDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'TopActionsDemoWebPartStrings' {
+  const strings: ITopActionsDemoWebPartStrings;
+  export = strings;
+}
