5db4738b991f1388f8e8503066226d314ea08a0f
Paolo Pialorsi <paolo@pialorsi.com>
2023-11-09 18:17:58 +0100

    Second commit of Security Tutorial samples

---
+++ Security-Tutorial/MISQLSecuredFunction/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISQLSecuredFunction
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
